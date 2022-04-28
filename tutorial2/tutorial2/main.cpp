32fcd61731fb93e09847c58f6522e55adff94ee4
Doniyorbek <104360232+LanceloT7730@users.noreply.github.com>
2022-04-28 14:42:10 +0500

    Xcode project was added
    description opening another xcode project

---
+++ tutorial2/tutorial2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tutorial2
+//
+//  Created by DONIYORBEK IBROKHIMOV on 28/04/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
